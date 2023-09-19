26ab52c4c34cfe3835db489f2324d46c904ddab7
xfmeng970526 <122116778+xfmeng970526@users.noreply.github.com>
2023-09-19 21:23:52 +0800

    Update bhxcy.js

--- bhxcy.js
+++ bhxcy.js
@@ -1,6 +1,8 @@
 /*
 渤海宣传员-微信小程序 v2.01
-
+cron: 25 8,21 * * *
+ 
+new Env('47-渤海宣传员-微信小程序');
 签到积分换E卡，每天跑一到两次
 捉小程序里的uid和token填到bhxcytoken里，多账号换行或者@隔开，格式如下
 export bhxcytoken="uid=1234567&token=qweqwertyuio"
@@ -8,7 +10,7 @@ export bhxcytoken="uid=1234567&token=qweqwertyuio"
 重写: 打开渤海宣传员-微信小程序
 [task_local]
 #渤海宣传员
-44 7,18 * * * https://raw.githubusercontent.com/leafTheFish/DeathNote/main/bhxcy.js, tag=渤海宣传员, enabled=true
+https://raw.githubusercontent.com/leafTheFish/DeathNote/main/bhxcy.js, tag=渤海宣传员, enabled=true
 [rewrite_local]
 https://gms.ihaoqu.com/gmswx/app.php url script-request-body https://raw.githubusercontent.com/leafTheFish/DeathNote/main/bhxcy.js
 [MITM]
